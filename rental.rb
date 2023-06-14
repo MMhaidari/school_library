a0beb520a445475fd3c46e50cc767551a82564ec
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-06-14 13:13:02 +0430

    rentals pursing data added to the project

--- rental.rb
+++ rental.rb
@@ -8,4 +8,28 @@ class Rental
     book.rentals << self
     person.rentals << self
   end
+
+  def to_json(*_args)
+    {
+      'date' => @date,
+      'book' => @book,
+      'person' => @person
+    }.to_json
+  end
+
+  def self.from_json(json_data, book, people)
+    rental_data = JSON.parse(json_data)
+    book = book.find { |b| b.title == rental_data['book']['title'] }
+    person = people.find { |p| p.name == rental_data['person']['name'] }
+
+    Rental.new(rental_data['date'], book, person)
+  end
+
+  def self.find_by_book(book, rentals)
+    rentals.select { |r| r.book == book }
+  end
+
+  def self.find_by_person(person, rentals)
+    rentals.select { |r| r.person == person }
+  end
 end
