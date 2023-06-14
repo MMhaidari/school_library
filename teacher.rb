d8e7f3b1da36416071be570a0fc108f043f43f3a
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-06-14 13:16:41 +0430

    teacher pursing data added to the project

--- teacher.rb
+++ teacher.rb
@@ -11,4 +11,21 @@ class Teacher < Person
   def can_use_services?
     true
   end
+
+  def to_json(*_args)
+    {
+      'type' => 'Teacher',
+      'name' => @name,
+      'age' => @age,
+      'parent_permission' => @parent_permission,
+      'specialization' => @specialization
+    }.to_json
+  end
+
+  def self.from_json(json_data)
+    teacher_data = JSON.parse(json_data)
+    Teacher.new(age: teacher_data['age'], name: teacher_data['name'],
+                parent_permission: teacher_data['parent_permission'],
+                specialization: teacher_data['specialization'])
+  end
 end
