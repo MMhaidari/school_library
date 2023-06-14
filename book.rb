db1560b2542314a86724a0fa55beac21c711a514
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-06-14 13:19:00 +0430

    book pursing data added to the project

--- book.rb
+++ book.rb
@@ -10,4 +10,17 @@ class Book
   def add_rental(date, person)
     Rental.new(date, self, person)
   end
+
+  def to_json(*_args)
+    {
+      'title' => @title,
+      'author' => @author,
+      'rentals' => @rentals
+    }.to_json
+  end
+
+  def self.from_json(json_data)
+    book_data = JSON.parse(json_data)
+    Book.new(book_data['title'], book_data['author'])
+  end
 end
