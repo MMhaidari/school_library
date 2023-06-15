30f02ed295d26d35af1561be4ba95a5a721465ed
FrankMugagga <frank.mugagga@gmail.com>
2023-06-15 12:06:56 +0300

    Unit testing for the Teacher class

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../classes/teacher'
+require 'rspec'
+require 'json'
+
+describe Teacher do
+    context "Create a person of type teacher"do
+      before(:each) do
+        @teacher = Teacher.new(age: 32, name: "Zed",parent_permission: true, specialization:'science')
+      end
+
+      it "Create a teacher with 'age, name, and specialization' " do
+        expect(@teacher).to have_attributes(age: 32)
+        expect(@teacher).to have_attributes(name: 'Zed') 
+        expect(@teacher).to have_attributes(specialization: 'science') 
+      end
+
+      it "Method 'can_use_services?' to be true" do
+        expect(@teacher.can_use_services?).to be true
+      end
+
+      it "Teacher conversion 'to json'" do
+        json_data = @teacher.to_json
+        expect(JSON.parse(json_data)).to eq({
+          "type" => "Teacher",
+          "name" => "Zed",
+          "parent_permission" => true,
+          "age" => 32,
+          "specialization" => "science"
+        })  
+      end
+      
+    end
+end
